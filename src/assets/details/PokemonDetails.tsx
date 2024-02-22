7d3acd80bef02ccbf14ab7673a5123c06aa5d782
marboqmar <martamartinezboquer@gmail.com>
2024-02-22 18:02:11 +0100

    Added comments

--- src/assets/details/PokemonDetails.tsx
+++ src/assets/details/PokemonDetails.tsx
@@ -6,6 +6,7 @@ import {getImage} from "../../App.tsx";
 
 const apiURL: string = "https://pokeapi.co/api/v2/pokemon/";
 
+// Parser
 const mapPokemonDetailsApiToPokemonDetails = (pokemon: PokemonDetailsFromApi[]): PokemonParsedDetails[] => {
     return pokemon.map((pokemonItem: PokemonDetailsFromApi) => {
         console.log(pokemonItem.name)
@@ -15,6 +16,7 @@ const mapPokemonDetailsApiToPokemonDetails = (pokemon: PokemonDetailsFromApi[]):
     });
 };
 
+// Call API, use parser and safe info to "pokemon"
 export const PokemonDetails = () => {
     const { pokemonId } = useParams();
     const [pokemon, setPokemon] = useState<PokemonParsedDetails[]>([]);
